1c7e5740c992c403512ab84d266367720dd5901b
Nicolae <bircanicolae98@gmail.com>
2025-01-30 22:54:48 +0100

    Updated files

---
+++ src/components/Categories.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ["All", "Meat", "Vegetarian", "Grill", "Spicy", "Closed"];
+
+    return (
+      <div className="categories">
+        <ul>
+          {categories.map((value, i) => (
+            <li
+              key={i}
+              onClick={() => setActiveIndex(i)}
+              className={activeIndex === i ? "active" : ''}
+            >
+              {value}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  
+export default Categories;
